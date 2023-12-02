34483449176e2d2373dce5974b77d488dab2aa8c
Kristóf Pásztor <pasztor.kristof.kp@gmail.com>
2023-12-03 00:29:10 +0100

    Search every workspace document for the definition

--- src/definition.ts
+++ src/definition.ts
@@ -6,20 +6,37 @@ export class TurtleDefinitionProvider implements DefinitionProvider {
     provideDefinition(document: TextDocument, position: Position): MatchLocation {
         const symbol = document.getText(document.getWordRangeAtPosition(position))
         const regex = definitionRegex(symbol)
-        for (let doc of turtleDocuments()) {
-            const result = findMatchInDocument(doc, regex, symbol)
-            if (result) {
-                return result
+        return findTurtleDocuments().then(documents => {
+            for (let doc of documents) {
+                const result = findMatchInDocument(doc, regex)
+                if (result) {
+                    return result
+                }
             }
-        }
-        return undefined
+            return undefined
+        })
     }
 }
 
 const definitionRegex = (str: string) => new RegExp(`(?<=\\.\\s*)\\b${str}\\b`, 'g')
-const turtleDocuments = () => workspace.textDocuments.filter(d => d.languageId === 'turtle')
 
-function findMatchInDocument(doc: TextDocument, regex: RegExp, symbol: string): MatchLocation {
+async function findTurtleDocuments(): Promise<TextDocument[]> {
+    const folders = workspace.workspaceFolders;
+    if (!folders) {
+        return [];
+    }
+
+    const files: Uri[] = [];
+    for (const folder of folders) {
+        const folderFiles = await workspace.findFiles(new RelativePattern(folder, '**/*.ttl'));
+        files.push(...folderFiles);
+    }
+
+    const documentPromises = files.map(async file => await workspace.openTextDocument(file));
+    return await Promise.all(documentPromises);
+}
+
+function findMatchInDocument(doc: TextDocument, regex: RegExp): MatchLocation {
     const text = doc.getText()
     
     const match = regex.exec(text)
@@ -29,13 +46,14 @@ function findMatchInDocument(doc: TextDocument, regex: RegExp, symbol: string): 
 
     const textBeforeMatch = text.substring(0, match.index)
     const lineNumber = textBeforeMatch.match(/\n/g)?.length || 0
-
+    
+    const symbol = match[0]
     const startIndex = doc.lineAt(lineNumber).text.indexOf(symbol)
     if (startIndex === -1) {
         return undefined
     }
 
     const start = new Position(lineNumber, startIndex)
-    const end = new Position(lineNumber, startIndex + match[0].length)
+    const end = new Position(lineNumber, startIndex + symbol.length)
     return new Location(doc.uri, new Range(start, end))
 }
