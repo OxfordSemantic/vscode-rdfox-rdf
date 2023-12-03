1c5333e7474a6bf773b8c48f13d26f62e6f90e6a
Kristóf Pásztor <pasztor.kristof.kp@gmail.com>
2023-12-03 11:37:51 +0100

    Parse prefix mapping to find definition

--- src/definition.ts
+++ src/definition.ts
@@ -1,14 +1,14 @@
 import { Definition, DefinitionProvider, Location, LocationLink, Position, ProviderResult, Range, RelativePattern, TextDocument, Uri, workspace } from 'vscode'
 
-type MatchLocation = ProviderResult<Definition | LocationLink[]> | null
+type MatchLocation = ProviderResult<Definition | LocationLink[]> | undefined
 
 export class TurtleDefinitionProvider implements DefinitionProvider {
     provideDefinition(document: TextDocument, position: Position): MatchLocation {
-        const symbol = document.getText(document.getWordRangeAtPosition(position))
-        const regex = definitionRegex(symbol)
+        const word = document.getText(document.getWordRangeAtPosition(position))
+        const identifier = new Identifier(word, getPrefixMapping(document.getText()))
         return findTurtleDocuments().then(documents => {
             for (let doc of documents) {
-                const result = findMatchInDocument(doc, regex)
+                const result = findMatchInDocument(doc, identifier)
                 if (result) {
                     return result
                 }
@@ -18,7 +18,38 @@ export class TurtleDefinitionProvider implements DefinitionProvider {
     }
 }
 
-const definitionRegex = (str: string) => new RegExp(`(?<=\\.\\s*)\\b${str}\\b`, 'g')
+class Identifier {
+    private readonly prefix: string
+    private readonly localName: string
+    
+    constructor(word: string, mapping: Map<string, string>) {
+        const trimmed = word.trim()
+        this.prefix = mapping.get(trimmed.substring(0, trimmed.indexOf(':'))) || ''
+        this.localName = word.substring(trimmed.indexOf(':') + 1, trimmed.length)
+    }
+    
+    getRegexForMapping(mapping: Map<string, string>): RegExp {
+        const entry = Array.from(mapping.entries()).filter(e => e[1] === this.prefix)[0]
+        if (!entry) {
+            return definitionRegex(`<${this.prefix}${this.localName}>`)
+        }
+        return definitionRegex(`(${entry[0]}:${this.localName})|(<${this.prefix}${this.localName}>)`)
+    }
+}
+
+const definitionRegex = (str: string) => new RegExp(`(?<=(\\.|^)\\s*)(${str})`, 'g')
+
+function getPrefixMapping(text: string): Map<string, string> {
+    const resultMap = new Map<string, string>()
+    prefixes(text)?.map(str => str.trim()).forEach(str => {
+        const key = str.substring(0, str.indexOf(':'))
+        const value = str.substring(str.indexOf('<') + 1, str.indexOf('>'))
+        resultMap.set(key, value)
+    })
+    return resultMap
+}
+
+const prefixes = (str: string) => /(?<=@prefix\s*)[a-zA-z0-9_-]*\s*:\s*<.+>(?=\s*\.)/g.exec(str)
 
 async function findTurtleDocuments(): Promise<TextDocument[]> {
     const folders = workspace.workspaceFolders
@@ -37,9 +68,10 @@ async function findTurtleDocuments(): Promise<TextDocument[]> {
     return await Promise.all(documentPromises)
 }
 
-function findMatchInDocument(doc: TextDocument, regex: RegExp): MatchLocation {
+function findMatchInDocument(doc: TextDocument, identifier: Identifier): MatchLocation {
     const text = doc.getText()
     
+    const regex = identifier.getRegexForMapping(getPrefixMapping(text))
     const match = regex.exec(text)
     if (!match) {
         return undefined
